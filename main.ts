3b3fa1536979ed6b9dbe6ce0ff265c33940ad2e9
GGiscool <80179694+924684@users.noreply.github.com>
2021-05-18 14:32:37 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,323 @@
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (Steve.isHittingTile(CollisionDirection.Bottom)) {
+        Steve.vy = 0 - Math.sqrt(9600)
+    }
+})
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (tiles.tileAtLocationEquals(tiles.locationOfSprite(Cursor), assets.tile`transparency16`)) {
+        tiles.setWallAt(tiles.locationOfSprite(Cursor), true)
+        tiles.setTileAt(tiles.locationOfSprite(Cursor), assets.tile`tile7`)
+    }
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    tiles.setWallAt(tiles.locationOfSprite(Cursor), false)
+    tiles.setTileAt(tiles.locationOfSprite(Cursor), assets.tile`transparency16`)
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    Steve,
+    [img`
+        . . . . . . . . . . . . . . . . 
+        . . . . c c c c . . . . . . . . 
+        . . c c 5 5 5 5 c c . . . . . . 
+        . c 5 5 5 5 5 5 5 5 c . . . . . 
+        c 5 5 5 5 5 1 f 5 5 5 c . . . . 
+        c 5 5 5 5 5 f f 5 5 5 5 c . . . 
+        c 5 5 5 5 5 5 5 5 5 5 5 c . . . 
+        c c b b 1 b 5 5 5 5 5 5 d c . . 
+        c 5 3 3 3 5 5 5 5 5 d d d c . . 
+        . b 5 5 5 5 5 5 5 5 d d d c . . 
+        . . c b b c 5 5 b d d d d c c . 
+        . c b b c 5 5 b b d d d d c d c 
+        . c c c c c c d d d d d d d d c 
+        . . . c c c c d 5 5 b d d d c . 
+        . . c c c c c b 5 5 b c c c . . 
+        . . c b b b c d 5 5 b c . . . . 
+        `,img`
+        . . . . c c c c c . . . . . . . 
+        . . c c 5 5 5 5 5 c . . . . . . 
+        . c 5 5 5 5 1 f 5 5 c . . . . . 
+        c 5 5 5 5 5 f f 5 5 5 c . . . . 
+        c 5 5 5 5 5 5 5 5 5 5 5 c . . . 
+        c c b b 1 b 5 5 5 5 5 5 c . . . 
+        c 5 3 3 3 5 5 5 5 5 5 5 d c . . 
+        c 5 3 3 3 5 5 5 5 5 d d d c . . 
+        . c 5 5 5 5 b 5 5 5 d d d c . . 
+        . . c b b c 5 5 b d d d d c . . 
+        . c b b c 5 5 b b d d d d c c c 
+        . c c c c c c d d d d d d d d c 
+        . . . . c c c b 5 5 b d d d c . 
+        . . . . . c d 5 5 b b c c c . . 
+        . . . . c c c c c c c . . . . . 
+        . . . . c b b b c . . . . . . . 
+        `,img`
+        . . . . c c c c c . . . . . . . 
+        . . c c 5 5 5 5 5 c . . . . . . 
+        . c 5 5 5 5 1 f 5 5 c . . . . . 
+        c 5 5 5 5 5 f f 5 5 5 c . . . . 
+        c 5 5 5 5 5 5 5 5 5 5 5 c . . . 
+        c c b b 1 b 5 5 5 5 5 5 c . . . 
+        c 5 3 3 3 5 5 5 5 5 5 5 d c . . 
+        c 5 5 5 5 5 5 5 5 5 d d d c . . 
+        . c 5 5 5 5 b 5 5 5 d d d c . . 
+        . . c b b c 5 5 b d d d d c . . 
+        . c b b c 5 5 b b d d d d c c c 
+        . c c c c c c d d d d d d d d c 
+        . . . . c c b 5 5 b d d d c c . 
+        . . . . c d 5 5 b b c c c . . . 
+        . . . . c c c c c c c . . . . . 
+        . . . . c b b b c . . . . . . . 
+        `,img`
+        . . . . c c c c c . . . . . . . 
+        . . c c 5 5 5 5 5 c . . . . . . 
+        . c 5 5 5 5 1 f 5 5 c . . . . . 
+        c 5 5 5 5 5 f f 5 5 5 c . . . . 
+        c 5 5 5 5 5 5 5 5 5 5 5 c . . . 
+        c c b b 1 b 5 5 5 5 5 5 c . . . 
+        c 5 3 3 3 5 5 5 5 5 5 5 d c . . 
+        c 5 5 5 5 5 5 5 5 5 d d d c . . 
+        . c 5 5 5 5 b 5 5 5 d d d c . . 
+        . . c b b c 5 5 b d d d d c . . 
+        . c b b c 5 5 b b d d d d c c . 
+        . c c c c c b b d d d d d d c c 
+        . . . c c 5 5 b 5 5 d d d d d c 
+        . . . . c b 5 5 b b c c c c c c 
+        . . . . c c c c c c . . . . . . 
+        . . . . . c b b b c . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . c c c c . . . . . . . . 
+        . . c c 5 5 5 5 c c . . . . . . 
+        . c 5 5 5 5 5 5 5 5 c . . . . . 
+        c 5 5 5 5 5 1 f 5 5 5 c . . . . 
+        c 5 5 5 5 5 f f 5 5 5 5 c . . . 
+        c c b b 1 b 5 5 5 5 5 5 c . . . 
+        c c 3 3 b b 5 5 5 5 5 5 d c . . 
+        c 5 3 3 3 5 5 5 5 5 d d d c . . 
+        . b 5 5 5 5 5 5 5 5 d d d c . . 
+        . . c b b c 5 5 b d d d d c . . 
+        . c b b c 5 5 b b d d d d c c c 
+        . c c c c c c d d d d d d d d c 
+        . . . c c c c d 5 5 b d d d c c 
+        . . . c b c c b 5 5 b c c c . . 
+        . . . c c c d 5 5 b c . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . c c c c . . . . . . . . 
+        . . c c 5 5 5 5 c c . . . . . . 
+        . c 5 5 5 5 5 5 5 5 c . . . . . 
+        c 5 5 5 5 5 1 f 5 5 5 c . . . . 
+        c 5 5 5 5 5 f f 5 5 5 5 c . . . 
+        c 5 5 5 5 5 5 5 5 5 5 5 c . . . 
+        c c b b 1 b 5 5 5 5 5 5 d c . . 
+        c 5 3 3 3 5 5 5 5 5 d d d c . . 
+        . b 5 5 5 5 5 5 5 5 d d d c . . 
+        . . c b b c 5 5 b d d d d c . . 
+        . c b b c 5 5 b b d d d d c c c 
+        . c c c c c c d d d d d d d d c 
+        . . . c c c c d 5 5 b d d c c . 
+        . . c b b c c c 5 5 b c c . . . 
+        . . c c c c c d 5 5 c . . . . . 
+        `],
+    200,
+    true
+    )
+    controller.moveSprite(Steve, 100, 0)
+})
+controller.combos.attachCombo("LL", function () {
+    pause(5)
+    controller.moveSprite(Steve, 150, 0)
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    controller.moveSprite(Steve, 100, 0)
+    animation.runImageAnimation(
+    Steve,
+    [img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . c c c c . . . . 
+        . . . . . . c c 5 5 5 5 c c . . 
+        . . . . . c 5 5 5 5 5 5 5 5 c . 
+        . . . . c 5 5 5 f 1 5 5 5 5 5 c 
+        . . . c 5 5 5 5 f f 5 5 5 5 5 c 
+        . . . c 5 5 5 5 5 5 5 5 5 5 5 c 
+        . . c d 5 5 5 5 5 5 b 1 b b c c 
+        . . c d d d 5 5 5 5 5 3 3 3 5 c 
+        . . c d d d 5 5 5 5 5 5 5 5 b . 
+        . c c d d d d b 5 5 c b b c . . 
+        c d c d d d d b b 5 5 c b b c . 
+        c d d d d d d d d c c c c c c . 
+        . c d d d b 5 5 d c c c c . . . 
+        . . c c c b 5 5 b c c c c c . . 
+        . . . . c b 5 5 d c b b b c . . 
+        `,img`
+        . . . . . . . c c c c c . . . . 
+        . . . . . . c 5 5 5 5 5 c c . . 
+        . . . . . c 5 5 f 1 5 5 5 5 c . 
+        . . . . c 5 5 5 f f 5 5 5 5 5 c 
+        . . . c 5 5 5 5 5 5 5 5 5 5 5 c 
+        . . . c 5 5 5 5 5 5 b 1 b b c c 
+        . . c d 5 5 5 5 5 5 5 3 3 3 5 c 
+        . . c d d d 5 5 5 5 5 3 3 3 5 c 
+        . . c d d d 5 5 5 b 5 5 5 5 c . 
+        . . c d d d d b 5 5 c b b c . . 
+        c c c d d d d b b 5 5 c b b c . 
+        c d d d d d d d d c c c c c c . 
+        . c d d d b 5 5 b c c c . . . . 
+        . . c c c b b 5 5 d c . . . . . 
+        . . . . . c c c c c c c . . . . 
+        . . . . . . . c b b b c . . . . 
+        `,img`
+        . . . . . . . c c c c c . . . . 
+        . . . . . . c 5 5 5 5 5 c c . . 
+        . . . . . c 5 5 f 1 5 5 5 5 c . 
+        . . . . c 5 5 5 f f 5 5 5 5 5 c 
+        . . . c 5 5 5 5 5 5 5 5 5 5 5 c 
+        . . . c 5 5 5 5 5 5 b 1 b b c c 
+        . . c d 5 5 5 5 5 5 5 3 3 3 5 c 
+        . . c d d d 5 5 5 5 5 5 5 5 5 c 
+        . . c d d d 5 5 5 b 5 5 5 5 c . 
+        . . c d d d d b 5 5 c b b c . . 
+        c c c d d d d b b 5 5 c b b c . 
+        c d d d d d d d d c c c c c c . 
+        . c c d d d b 5 5 b c c . . . . 
+        . . . c c c b b 5 5 d c . . . . 
+        . . . . . c c c c c c c . . . . 
+        . . . . . . . c b b b c . . . . 
+        `,img`
+        . . . . . . . c c c c c . . . . 
+        . . . . . . c 5 5 5 5 5 c c . . 
+        . . . . . c 5 5 f 1 5 5 5 5 c . 
+        . . . . c 5 5 5 f f 5 5 5 5 5 c 
+        . . . c 5 5 5 5 5 5 5 5 5 5 5 c 
+        . . . c 5 5 5 5 5 5 b 1 b b c c 
+        . . c d 5 5 5 5 5 5 5 3 3 3 5 c 
+        . . c d d d 5 5 5 5 5 5 5 5 5 c 
+        . . c d d d 5 5 5 b 5 5 5 5 c . 
+        . . c d d d d b 5 5 c b b c . . 
+        . c c d d d d b b 5 5 c b b c . 
+        c c d d d d d d b b c c c c c . 
+        c d d d d d 5 5 b 5 5 c c . . . 
+        c c c c c c b b 5 5 b c . . . . 
+        . . . . . . c c c c c c . . . . 
+        . . . . . . c b b b c . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . c c c c . . . . 
+        . . . . . . c c 5 5 5 5 c c . . 
+        . . . . . c 5 5 5 5 5 5 5 5 c . 
+        . . . . c 5 5 5 f 1 5 5 5 5 5 c 
+        . . . c 5 5 5 5 f f 5 5 5 5 5 c 
+        . . . c 5 5 5 5 5 5 b 1 b b c c 
+        . . c d 5 5 5 5 5 5 b b 3 3 c c 
+        . . c d d d 5 5 5 5 5 3 3 3 5 c 
+        . . c d d d 5 5 5 5 5 5 5 5 b . 
+        . . c d d d d b 5 5 c b b c . . 
+        c c c d d d d b b 5 5 c b b c . 
+        c d d d d d d d d c c c c c c . 
+        c c d d d b 5 5 d c c c c . . . 
+        . . c c c b 5 5 b c c b c . . . 
+        . . . . . c b 5 5 d c c c . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . c c c c . . . . 
+        . . . . . . c c 5 5 5 5 c c . . 
+        . . . . . c 5 5 5 5 5 5 5 5 c . 
+        . . . . c 5 5 5 f 1 5 5 5 5 5 c 
+        . . . c 5 5 5 5 f f 5 5 5 5 5 c 
+        . . . c 5 5 5 5 5 5 5 5 5 5 5 c 
+        . . c d 5 5 5 5 5 5 b 1 b b c c 
+        . . c d d d 5 5 5 5 5 3 3 3 5 c 
+        . . c d d d 5 5 5 5 5 5 5 5 b . 
+        . . c d d d d b 5 5 c b b c . . 
+        c c c d d d d b b 5 5 c b b c . 
+        c d d d d d d d d c c c c c c . 
+        . c c d d b 5 5 d c c c c . . . 
+        . . . c c b 5 5 c c c b b c . . 
+        . . . . . c 5 5 d c c c c c . . 
+        `],
+    200,
+    true
+    )
+})
+controller.combos.attachCombo("RR", function () {
+    pause(5)
+    controller.moveSprite(Steve, 150, 0)
+})
+function MakeLandscape () {
+    LandHight = 8
+    for (let XIndex = 0; XIndex <= 31; XIndex++) {
+        if (Math.percentChance(HillHightRange / 2)) {
+            LandHight = LandHight - 1
+        }
+        if (Math.percentChance(HillHightRange / 2)) {
+            LandHight = LandHight - 1
+        }
+        if (Math.percentChance(HillHightRange / 2)) {
+            LandHight = LandHight + 1
+        }
+        if (Math.percentChance(HillHightRange / 2)) {
+            LandHight = LandHight + 1
+        }
+        tiles.setTileAt(tiles.getTileLocation(XIndex, LandHight), assets.tile`tile2`)
+        tiles.setWallAt(tiles.getTileLocation(XIndex, LandHight), true)
+        for (let YIndex = 0; YIndex <= 31 - LandHight; YIndex++) {
+            tiles.setTileAt(tiles.getTileLocation(XIndex, 32 - YIndex), assets.tile`tile3`)
+            tiles.setWallAt(tiles.getTileLocation(XIndex, 32 - YIndex), true)
+        }
+    }
+}
+let LandHight = 0
+let Cursor: Sprite = null
+let Steve: Sprite = null
+let HillHightRange = 0
+// From 0 to 100. The higher the number, the bumpier the terrain. 0 means a superflatworld.
+HillHightRange = 20
+tiles.setTilemap(tilemap`level_0`)
+MakeLandscape()
+scene.setBackgroundColor(15)
+Steve = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . c c c c . . . . . . . . 
+    . . c c 5 5 5 5 c c . . . . . . 
+    . c 5 5 5 5 5 5 5 5 c . . . . . 
+    c 5 5 5 5 5 1 f 5 5 5 c . . . . 
+    c 5 5 5 5 5 f f 5 5 5 5 c . . . 
+    c 5 5 5 5 5 5 5 5 5 5 5 c . . . 
+    c c b b 1 b 5 5 5 5 5 5 d c . . 
+    c 5 3 3 3 5 5 5 5 5 d d d c . . 
+    . b 5 5 5 5 5 5 5 5 d d d c . . 
+    . . c b b c 5 5 b d d d d c c . 
+    . c b b c 5 5 b b d d d d c d c 
+    . c c c c c c d d d d d d d d c 
+    . . . c c c c d 5 5 b d d d c . 
+    . . c c c c c b 5 5 b c c c . . 
+    . . c b b b c d 5 5 b c . . . . 
+    `, SpriteKind.Player)
+scene.cameraFollowSprite(Steve)
+controller.moveSprite(Steve, 100, 0)
+Steve.ay = 300
+tiles.placeOnTile(Steve, tiles.getTileLocation(1, 2))
+Cursor = sprites.create(img`
+    . e . 
+    e e e 
+    . e . 
+    `, SpriteKind.Player)
+Cursor.setFlag(SpriteFlag.StayInScreen, true)
+Cursor.setFlag(SpriteFlag.Ghost, true)
+let CursorX = 8
+let CursorY = 8
+game.onUpdate(function () {
+    if (controller.player2.isPressed(ControllerButton.Left)) {
+        CursorX += -1
+    }
+    if (controller.player2.isPressed(ControllerButton.Right)) {
+        CursorX += 1
+    }
+    if (controller.player2.isPressed(ControllerButton.Up)) {
+        CursorY += -1
+    }
+    if (controller.player2.isPressed(ControllerButton.Down)) {
+        CursorY += 1
+    }
+    Cursor.setPosition(scene.cameraProperty(CameraProperty.X) + CursorX, scene.cameraProperty(CameraProperty.Y) + CursorY)
+})
